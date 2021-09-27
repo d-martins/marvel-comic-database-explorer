d58ffbdf000a6a71bfc37d78f7934ceaddcfb47d
Diogo.Martins <Diogo.Martins@losch.lu>
2021-09-27 18:56:58 +0100

    feat: included bulma in project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
 
-const Home: NextPage = ({children}) => {
+const Home: NextPage = ({ children }) => {
   return (
     <div className={styles.container}>
       <Head>
         <title>Ultimate Marvel Database Explorer</title>
         <meta name="description" content="Discover Marvel comics" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Red+Hat+Display:wght@400;500;700&display=swap" rel="stylesheet"/>
       </Head>
 
       <main className={styles.main}>
@@ -20,7 +21,7 @@ const Home: NextPage = ({children}) => {
       </main>
 
       <footer className={styles.footer}>
-      <a href="http://marvel.com">Data provided by Marvel. © 2021 MARVEL</a>
+        <a href="http://marvel.com">Data provided by Marvel. © 2021 MARVEL</a>
       </footer>
     </div>
   )
