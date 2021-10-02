329f816893db7392413f7e7acdf052f55a81f94d
Diogo.Martins <Diogo.Martins@losch.lu>
2021-10-02 16:58:23 +0100

    Created layout to hold the filter options - made in a way that the layout of the list can easily change dinamically (card to list for example)

---
+++ components/FilterLayout/FilterLayout.tsx
@@ -0,0 +1,33 @@
+import { FC, ReactNode } from "react"
+import Dropdown, { DropdownProps } from "../Dropdown/Dropdown";
+import SearchTypeAhead, { SearchTypeAheadProps } from "../SearchBar/SearchTypeAhead";
+import styles from './FilterLayout.module.scss';
+
+
+export type FilterLayoutProps = {
+    filters: DropdownProps[]
+    searchOptions: SearchTypeAheadProps
+}
+
+const FilterLayout: FC<FilterLayoutProps> = ({ children, searchOptions, filters }) => {
+
+    return (
+        <div className={`container ${styles["filter-layout"]}`}>
+            <div className="filter-zones is-flex-tablet is-justify-content-space-between" >
+                <div className="search-zone flex-shrink is-fullwidth">
+                    <SearchTypeAhead {...searchOptions}></SearchTypeAhead>
+                </div>
+                <div className="filters-zone flex-grow">
+                    {filters.map((filterProps) => {
+                        return <Dropdown {...filterProps} key={filterProps.label}></Dropdown>
+                    })}
+                </div>
+            </div>
+            <div className="content-zone">
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export default FilterLayout
